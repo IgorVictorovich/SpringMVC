73a5970c045fd6666fd759a383d23a77dd85f301
Igor <russion2005@gmail.com>
2015-05-06 22:17:43 +0300

    added getShiffledCoffeeLovers to model

--- src/main/java/ua/com/otpbank/controller/MailController.java
+++ src/main/java/ua/com/otpbank/controller/MailController.java
@@ -1,9 +1,11 @@
 package ua.com.otpbank.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import ua.com.otpbank.service.Generator;
 
 /**
  * Created by Igor on 04.05.2015.
@@ -11,8 +13,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/send_result")
 public class MailController {
+    @Autowired
+    private Generator generator = Generator.getInstance();
+
     @RequestMapping(method = RequestMethod.POST)
     public String doPost(ModelMap model){
+
+        model.addAttribute("shuffledList", generator.getShuffledCoffeeLovers());
+      //  model.addAttribute("getList", generator.getCoffeeLovers());
         return "send_result";
     }
 }
